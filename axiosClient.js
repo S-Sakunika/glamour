f96b97ba4f148c5f2b9c7e873bf389857ab9fb6c
S-Sakunika <31379863+S-Sakunika@users.noreply.github.com>
2023-07-24 23:29:43 +0530

    Update base URL to secure

--- axiosClient.js
+++ axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: 'http://makeup-api.herokuapp.com/api/v1/products.json'
+    baseURL: 'https://makeup-api.herokuapp.com/api/v1/products.json'
 })
 
 export default axiosClient
